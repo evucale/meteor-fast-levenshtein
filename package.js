9d5aa8f5d792547e2dc85703398818dacc6fb1b7
Calvin Reibenspieß <calvinreibenspiess@gmail.com>
2015-07-05 00:22:26 +0200

    change package name and descriptions

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
-  name: 'levenshtein',
-  summary: "Efficient Levenshtein implementation with asynchronous callback support",
+  name: 'fast-levenshtein',
+  summary: "fast-levenshtein (inofficial): Efficient Levenshtein implementation with asynchronous callback support",
   version: "1.0.0",
 });
 
 Package.onUse(function (api) {
   api.versionsFrom("0.9.0");
   api.addFiles('levenshtein.js');
-  api.export('levenshtein');
+  api.export('Levenshtein');
 });
 
 Package.onTest(function(api) {
   api.use('sanjo:jasmine@0.14.0');
-  api.use('levenshtein');
+  api.use('fast-levenshtein');
   api.addFiles('meteorPackageTests.js');
 });
