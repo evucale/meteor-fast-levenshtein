6490981fc22a7bf50b70cde1eb39d9f06358e2c5
Calvin Reibenspieß <calvinreibenspiess@gmail.com>
2015-07-05 15:04:12 +0200

    enhanced tests

--- meteorPackageTests.js
+++ meteorPackageTests.js
@@ -1,7 +1,16 @@
-describe('levenshtein', function () {
-   it('should show the difference between two strings', function () {
-      expect(Levenshtein.get("boop","booking")).toBe(4);
-      expect(Levenshtein.get("archeologie","archéologique")).toBe(3);
-      expect(Levenshtein.get("baum","baum")).toBe(0);
+var testDistance = function(str1, str2, distance, percent){
+  describe('levenshtein: '+str1+" <> "+str2, function () {
+     it('should show the difference between two strings', function () {
+        expect(Levenshtein.get(str1,str2)).toBe(distance);
+        expect(Levenshtein.get(str2,str1)).toBe(distance);
+     });
+     it('should show the difference between two strings in percent', function () {
+       expect(Levenshtein.getPercent(str1,str2).toFixed(2)).toBe(percent);
+       expect(Levenshtein.getPercent(str2,str1).toFixed(2)).toBe(percent);
+     });
    });
- });
+};
+
+testDistance("boop","booking", 4, "0.43");
+testDistance("archéologique","archeologie", 3,"0.77");
+testDistance("baum","baum", 0, "1.00");
